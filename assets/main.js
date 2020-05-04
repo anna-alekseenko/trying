1e70592500c0cd9ef896f887570e5a4e03a4e926
Ann <anixenka@gmail.com>
2020-05-04 08:48:20 +0300

    Almost final version

--- assets/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-	this.setTimeout(showPage, 2000);
-}
-
-function showPage() {
-	document.getElementById("loader").classList.toggle('fade');
-	document.getElementById("content").classList.toggle('appear');
-}
-
